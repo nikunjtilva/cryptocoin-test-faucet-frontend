2befdc001fcb35dbec12c5423d9d516f2477be2c
Nikunj Tilva <nikunjltilva@gmail.com>
2018-08-23 13:20:38 +0200

    removed unused references

--- src/app/coin-selection/coin-selection.component.ts
+++ src/app/coin-selection/coin-selection.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-coin-selection',
   templateUrl: './coin-selection.component.html',
   styleUrls: ['./coin-selection.component.css']
 })
-export class CoinSelectionComponent implements OnInit,OnChanges {
+export class CoinSelectionComponent implements OnInit {
 
   @Input() coin='tbtc';
   @Output() coinChange = new EventEmitter<string>();  
